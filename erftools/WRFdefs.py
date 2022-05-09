1a2e846dbf1a4a35f73cb23125d87e854611c23d
Eliot Quon <eliot.quon@nrel.gov>
2022-05-09 15:42:10 -0600

    Implement WRFdefs.domains and inputs.ERFinput objects

--- erftools/WRFdefs.py
+++ erftools/WRFdefs.py
@@ -7,7 +7,7 @@ class time_control(object):
 
     def __str__(self):
         return f"""WRF `time_control` parameters
-  sim range: {self.start_datetime} to {self.end_datetime} ({self.stop_time} s)"""
+  sim range: {self.start_datetime} to {self.end_datetime}"""
 
     def parse_datetime_range(self):
         # assume all domains have the same start/end datetime for now
@@ -22,6 +22,34 @@ class time_control(object):
         end_hour  = self.legacy['end_hour'][idom]
         self.start_datetime = datetime(start_year, start_month, start_day, start_hour)
         self.end_datetime = datetime(end_year, end_month, end_day, end_hour)
-        self.stop_time = (self.end_datetime - self.start_datetime).total_seconds()
         
     
+class domains(object):
+    def __init__(self,nmldict):
+        self.legacy = nmldict
+        self.parse_grid()
+
+    def __str__(self):
+        s = 'WRF `domains` parameters\n'
+        for dom in range(self.max_dom):
+            s += f'  domain d{dom+1:02d}:' \
+                 f' [(1,{self.e_we[dom]:d}),(1,{self.e_sn[dom]:d}),(1,{self.e_vert[dom]:d})]' \
+                 f' ds=[{self.dx[dom]},{self.dy[dom]}]\n'
+        return s
+
+    def parse_grid(self):
+        self.max_dom = self.legacy['max_dom']
+        # UNstaggered grid points == cells
+        # - assume s_we == s_sn == s_vert == [1, 1, ...]
+        self.e_we = self.legacy['e_we'][:self.max_dom] # west--east
+        self.e_sn = self.legacy['e_sn'][:self.max_dom] # south--north
+        self.e_vert = self.legacy['e_vert'][:self.max_dom] # bottom--top
+        self.dx = self.legacy['dx'][:self.max_dom]
+        self.dy = self.legacy['dy'][:self.max_dom]
+        self.i_parent_start = self.legacy['i_parent_start'][:self.max_dom]
+        self.j_parent_start = self.legacy['j_parent_start'][:self.max_dom]
+        parent_grid_ratio = self.legacy['parent_grid_ratio'][:self.max_dom]
+        for dom in range(1,self.max_dom):
+            assert (self.dx[dom-1]/self.dx[dom] == parent_grid_ratio[dom])
+            assert (self.dy[dom-1]/self.dy[dom] == parent_grid_ratio[dom])
+
--- erftools/preprocessing.py
+++ erftools/preprocessing.py
@@ -1,6 +1,7 @@
 import f90nml
 
-from .WRFdefs import time_control#, domains, physics
+from .WRFdefs import time_control, domains#, physics
+from .inputs import ERFinput
 
 class WRFnamelist(object):
     """Class to parse WRF inputs and convert to ERF inputs"""
@@ -9,9 +10,18 @@ class WRFnamelist(object):
         with open(nmlpath,'r') as f:
             self.nml = f90nml.read(f)
         self.time_control = time_control(self.nml['time_control'])
+        self.domains = domains(self.nml['domains'])
+        self.erf_input = ERFinput()
+        self.calculate_inputs()
 
     def __str__(self):
-        s = ''
-        s+= str(self.time_control)
+        s = str(self.time_control) + '\n'
+        s+= str(self.domains)+ '\n'
         return s
 
+    def calculate_inputs(self):
+        tdelta = self.time_control.end_datetime - self.time_control.start_datetime
+        self.erf_input['stop_time'] = tdelta.total_seconds()
+        n_cell = [self.domains.e_we[0], self.domains.e_sn[0], self.domains.e_vert[0]]
+        self.erf_input['amr.n_cell'] = n_cell
+        
