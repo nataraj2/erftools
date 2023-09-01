5635e1e248e7c44c3fcec3762e36dbb90d68f92a
Eliot Quon <eliot.quon@nrel.gov>
2023-09-01 10:29:04 -0600

    Add equation of state module following ERF's EOS.H

--- erftools/constants.py
+++ erftools/constants.py
@@ -3,6 +3,7 @@ R_d = 287.0         # gas constant of dry air [J/(kg K)]
 R_v = 461.6         # gas constant for water vapor [J/(kg K)]
 Cp_d = 7./2.*R_d    # specific heat of dry air at constant pressure [J/(kg K)]
 Cv_d = Cp_d - R_d   # specific heat of dry air at constant volume [J/(kg K)]
+Cp_v = 1859.0       # specific heat of water vapor at constant pressure [J/(kg K)]
 
 p_0 = 1e5           # reference surface pressure [Pa] == 1000 hPa
 Gamma = 1.4         # ratio of specific heats [-]
---
+++ erftools/eos.py
@@ -0,0 +1,31 @@
+import numpy as np
+from .constants import Gamma, p_0, R_d, R_v, Cp_d, Cp_v
+
+def getdPdRgivenConstantTheta(rho, theta):
+    return Gamma * p_0 * (R_d * theta / p_0)**Gamma * rho**(Gamma-1.0)
+
+def getPgivenRTh(rhotheta, qv=0, precip=True):
+    """Function to return pressure given density times theta
+
+    Parameters
+    ----------
+    rhotheta: float
+        Density times potential temperature
+    qv: float, optional
+        Water vapor mixing ratio
+    precip: bool
+        Precipitation is modeled by a single-moment microphysics model,
+        otherwise a warm moisture model is used.
+    """
+    if qv==0:
+        return p_0 * (R_d * rhotheta / p_0)**Gamma
+    elif not precip:
+        rhotheta_t = rhotheta*(1.0+qv);
+        return p_0 * (R_d * rhotheta_t / p_0)**Gamma
+    else:
+        R_t        =  R_d + qv*R_v;
+        Cp_t       = Cp_d + qv*Cp_v;
+        Gamma_t    = Cp_t / (Cp_t-R_t);
+        rhotheta_t = rhotheta*(1.0+qv);
+        return p_0 * (R_t * rhotheta_t / p_0)**Gamma_t
+
