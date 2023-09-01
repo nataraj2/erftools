0483c47956cddfed23fc44de7076c0385b17169b
Eliot Quon <eliot.quon@nrel.gov>
2023-09-01 09:34:16 -0600

    Rename constants to be consistent with ERF, add Gamma const
    g --> CONST_GRAV
    p0 --> p_0
    c_p --> Cp_d
    c_v --> Cv_d

--- erftools/constants.py
+++ erftools/constants.py
@@ -1,10 +1,9 @@
 # physical constants
-g = 9.81 # gravitational acceleration [m/s^2]
-R_d = 287.0 # gas constant of dry air [J/(kg K)]
-R_v = 461.6 # gas constant for water vapor [J/(kg K)]
-c_p = 7./2.*R_d # specific heat of dry air at constant pressure [J/(kg K)]
-c_v = c_p - R_d # specific heat of dry air at constant volume [J/(kg K)]
+R_d = 287.0         # gas constant of dry air [J/(kg K)]
+R_v = 461.6         # gas constant for water vapor [J/(kg K)]
+Cp_d = 7./2.*R_d    # specific heat of dry air at constant pressure [J/(kg K)]
+Cv_d = Cp_d - R_d   # specific heat of dry air at constant volume [J/(kg K)]
 
-# model constants
-p0 = 1e5 # pressure at 1000 hPa [Pa]
-
+p_0 = 1e5           # reference surface pressure [Pa] == 1000 hPa
+Gamma = 1.4         # ratio of specific heats [-]
+CONST_GRAV = 9.81   # gravitational acceleration [m/s^2]
