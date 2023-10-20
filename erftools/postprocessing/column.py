bad1357cbc6dd66e0f7648552e124abd51cd7507
Eliot Quon <eliot.quon@nrel.gov>
2023-10-20 10:44:32 -0600

    Add code to help postprocess pltfiles for column sims

---
+++ erftools/postprocessing/column.py
@@ -0,0 +1,91 @@
+import os
+import glob
+import numpy as np
+import pandas as pd
+import yt
+yt.set_log_level('error')
+
+def load_pltfile_column(dpath,return_amrex_dataset=False):
+    amrds = yt.load(dpath)
+    # get vertical levels
+    grid_spacing = (amrds.domain_right_edge.value
+                    - amrds.domain_left_edge.value) / amrds.domain_dimensions
+    zlevels = 0.5*grid_spacing[2] \
+            + np.arange(amrds.domain_dimensions[2]) * grid_spacing[2]
+    # extract column
+    col_left_edge = amrds.domain_left_edge.value
+    col_right_edge = [grid_spacing[0], grid_spacing[1], amrds.domain_right_edge.value[2]]
+    col = amrds.box(col_left_edge, col_right_edge)
+    # get column values
+    fields = [name for (dtype,name) in amrds.field_list if dtype=='boxlib']
+    coldata = {field: col[field].value for field in fields}
+    df = pd.DataFrame(coldata, index=pd.Index(zlevels, name='height'))
+    if return_amrex_dataset:
+        return df, amrds
+    else:
+        return df
+
+class Column(object):
+    """Helper for loading in pltfile data from a column simulation, generally
+    set up as a domain that has
+        amr.n_cell = amr.blocking_factor amr.blocking_factor n_levels
+        geometry.is_periodic = 1 1 0
+    """
+    def __init__(self,pltfiles,**kwargs):
+        """Read one or more pltfiles and convert to dataframe format
+
+        Parameters
+        ----------
+        pltfiles : str, list
+            May be specified as a globstring, a single pltfile path,
+            or a list of pltfile paths
+        """
+        if isinstance(pltfiles, str):
+            # first see if input is a globstring
+            globbed = glob.glob(pltfiles)
+            if len(globbed) > 0:
+                pltfiles = globbed
+            elif os.path.isdir(pltfiles):
+                pltfiles = [pltfiles]
+            else:
+                raise FileNotFoundError(
+                        f'{pltfiles} is neither a valid directory path'
+                         ' or glob string')
+        else:
+            # input checks
+            assert hasattr(pltfiles,'__iter__')
+            assert all([os.path.isdir(pltfile) for pltfile in pltfiles])
+        self.pltfiles = pltfiles
+        self._sort_pltfiles()
+        self._load_pltfile_wrapper()
+
+    def _sort_pltfiles(self):
+        if any(['.old.' in dpath for dpath in self.pltfiles]):
+            print('Warning: One or more input files is old -- ignoring')
+        pltfiles = [dpath for dpath in self.pltfiles
+                    if os.path.isdir(dpath) and (not '.old.' in dpath)]
+        pltnames = [os.path.split(dpath)[1] for dpath in pltfiles]
+        assert all([name.startswith('plt') for name in pltnames])
+        outputsteps = [int(name[3:]) for name in pltnames]
+        sortorder = np.argsort(outputsteps)
+        self.pltfiles = [pltfiles[i] for i in sortorder]
+
+    def _load_pltfile_wrapper(self):
+        dflist = []
+        times = []
+        for pltfile in self.pltfiles:
+            print('\rLoading',pltfile,end='')
+            df,amrds = load_pltfile_column(pltfile, return_amrex_dataset=True)
+            dflist.append(df)
+            times.append(amrds.current_time.value.item())
+        print('')
+        self.t = np.array(times)
+        tindex = pd.Index(times,name='time')
+        self._df = pd.concat(dflist, axis=0, keys=tindex)
+
+    @property
+    def df(self):
+        if len(self.t) == 1:
+            return self._df.xs(self.t[0],level='time')
+        else:
+            return self._df
